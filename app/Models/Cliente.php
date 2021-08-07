f0ea90aaba4106b0650fa56844efc5e2075c6774
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-06 21:53:41 -0300

    Set the fillable array and create method rules. The fillable array defines the properties to stored in the database. The method rules is used by validate() to validates the request data.

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nome',
+        'image',
+        'cpf_cnpj'
+    ];
+
+    public function rules()
+    {
+        return [
+            'nome'=>'required',
+            'image'=>'image',
+            'cpf_cnpj'=>'required|unique:clientes'
+        ];
+    }
 }
