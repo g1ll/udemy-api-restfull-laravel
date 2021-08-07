2e95bd0ca03623b91f785fba1e60863230bed3a3
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-06 21:26:55 -0300

    Comment previous route of clientes.

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('clientes',[ClienteApiController::class,'index']);
+// Route::get('clientes',[ClienteApiController::class,'index']);
+
 
