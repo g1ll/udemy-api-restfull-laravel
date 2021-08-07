dbb7f04f8126cd533d2e73fcd9f33e4666074def
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-06 21:26:41 -0300

    Create  a backup copie of Cliente controller.

---
+++ app/Http/Controllers/Api/ClienteApiController_bkp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteApiController extends Controller
+{
+    public function __construct(Cliente $cliente, Request $req)
+    {
+        $this->cliente = $cliente;
+        $this->req=$req;
+    }
+
+    public function index()
+    {
+        // return response()->json('Hello, World!!!');
+        $data = $this->cliente->all();
+        // dd($data);
+        return response()->json($data);
+    }
+}
