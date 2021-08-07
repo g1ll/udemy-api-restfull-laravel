b17158088ea3a8cc32ced05a5326a066f4a41c88
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-06 21:27:32 -0300

    Create a new controller with option --resource. Command: $>php artisan make:controller Api/ClienteApiController --resource

--- app/Http/Controllers/Api/ClienteApiController.php
+++ app/Http/Controllers/Api/ClienteApiController.php
@@ -4,21 +4,82 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Cliente;
 
 class ClienteApiController extends Controller
 {
-    public function __construct(Cliente $cliente, Request $req)
-    {
-        $this->cliente = $cliente;
-        $this->req=$req;
-    }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        // return response()->json('Hello, World!!!');
-        $data = $this->cliente->all();
-        // dd($data);
-        return response()->json($data);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
