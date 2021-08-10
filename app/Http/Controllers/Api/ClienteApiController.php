23a8fc936483d4920bdb21d2fd178a4fbff7ab0a
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-10 20:37:44 -0300

    Show error for invalid Id. Using call to method find.

--- app/Http/Controllers/Api/ClienteApiController.php
+++ app/Http/Controllers/Api/ClienteApiController.php
@@ -68,9 +68,13 @@ class ClienteApiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    // public function show(Cliente $cliente)
+    public function show($id)
     {
-        // $cliente = $this->cliente->find($id);
+        $cliente = $this->cliente->find($id);
+        if(!$cliente){
+            return response()->json(['error'=>'Id inválido!'],404);
+        }
         // $cliente = Cliente::find($id);
         return response()->json($cliente);
     }
