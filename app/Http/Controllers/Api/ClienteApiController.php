4fb7db82b941681d8678759589ed25397ffed966
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-12 18:04:32 -0300

    Remove extra codes and create new propertie.

--- app/Http/Controllers/Api/ClienteApiController.php
+++ app/Http/Controllers/Api/ClienteApiController.php
@@ -2,24 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\MainApiController;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
-use Intervention\Image\ImageManagerStatic as Image;
-use Illuminate\Support\Facades\Storage;
 
 class ClienteApiController extends MainApiController
 {
 
     protected $model;
     protected $path = 'cliente';
+    protected $upload = 'image';
 
     public function __construct(Cliente $clientes, Request $req)
     {
         $this->model = $clientes;
         $this->req = $req;
-        // $this->model = new Cliente();
     }
 
 
