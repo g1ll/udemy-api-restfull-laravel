45e2d214d605c68de8961c22dd0ababdb638f163
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-13 21:28:35 -0300

    Change destroy method to use file() from model.

--- app/Http/Controllers/MainApiController.php
+++ app/Http/Controllers/MainApiController.php
@@ -128,8 +128,8 @@ class MainApiController extends BaseController
         if (!$model = $this->model->find($id))
             return response()->json(['error' => 'Id inválido!'], 404);
 
-        if ($model->image)
-            Storage::disk('public')->delete("$this->path/$model->image");
+        if ($model->file())
+            Storage::disk('public')->delete("$this->path/{$model->file()}");
 
         $model->delete();
         return response()->json(['success' => 'registro removido!']);
