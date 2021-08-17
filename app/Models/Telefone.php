1a4b97dd7fe50dacf4ed4e672e2fb82b4f98585c
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-17 20:04:17 -0300

    Create telefone model with relationship of cliente

--- app/Models/Telefone.php
+++ app/Models/Telefone.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cliente;
 
 class Telefone extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'numero',
+        'cliente_id',
+    ];
+
+    public function rules()
+    {
+        return [
+            'cliente_id'=>'required',
+            'numero'=>'required|max:20'
+        ];
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class,'cliente_id','id');
+    }
 }
