89934347ef65e42fe82d302af115a5407b1dfb82
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-17 20:19:38 -0300

    Create Controller Telefone.

--- app/Http/Controllers/Api/TelefoneApiController.php
+++ app/Http/Controllers/Api/TelefoneApiController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Telefone;
+use App\Http\Controllers\MainApiController;
 use Illuminate\Http\Request;
 
-class TelefoneApiController extends Controller
+class TelefoneApiController extends MainApiController
 {
-    //
+    protected $model;
+    protected $upload;
+    protected $path;
+
+    public function __construct(Telefone $telefone, Request $req)
+    {
+        $this->model = $telefone;
+        $this->req = $req;
+    }
+
+    public function cliente($id)
+    {
+        $doc_ciente = $this->model->with('cliente')->find($id);
+        return response()->json(
+            (!$doc_ciente) ? ['error' => 'Id inválido!'] : $doc_ciente,
+            (!$doc_ciente) ? 404 : 200
+        );
+    }
 }
