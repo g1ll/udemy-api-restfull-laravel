b405f51497dedb4142fa927c9343759b0f1f63b4
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-17 21:52:39 -0300

    Create new controller for filmes.

---
+++ app/Http/Controllers/Api/FilmeApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\MainApiController;
+use Illuminate\Http\Request;
+use App\Models\Filme;
+
+class FilmeApiController extends MainApiController
+{
+    protected $model;
+    protected $path = 'filmes';
+    protected $upload = 'capa';
+    protected $width = 800;
+    protected $height = 533;
+
+    public function __construct(Filme $filme, Request $req)
+    {
+        $this->model = $filme;
+        $this->req = $req;
+        // dd($req);
+    }
+}
