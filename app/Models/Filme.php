c281ec360df2cd0bcffe3f32a834f6d7b59ac300
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-17 21:52:21 -0300

    Create model Filmes.

--- app/Models/Filme.php
+++ app/Models/Filme.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Filme extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'capa'
+    ];
+
+
+    public function rules()
+    {
+        return [
+            'titulo'=>'required',
+            'capa'=>'image',
+        ];
+    }
+
+    public function file($id)
+    {
+        $file = $this->find($id);
+        return $file->capa;
+    }
+
 }
