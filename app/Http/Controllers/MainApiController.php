75f3a1126cdc4b47bca70633614c7203a2f34d01
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-19 20:30:28 -0300

    Create new method to handler errors and validation

--- app/Http/Controllers/MainApiController.php
+++ app/Http/Controllers/MainApiController.php
@@ -46,10 +46,11 @@ class MainApiController extends BaseController
                 $imgName = uniqid(date('His')); //Unique ID based on date hour, minute and seconds.
                 $nameFile = "$imgName.$extension";
                 $upload = Image::make($this->req->file($this->upload))
-                                ->resize($this->width, $this->height)
-                                ->save(
-                                    storage_path("app/publics/$this->path/$nameFile"),
-                                    70);
+                    ->resize($this->width, $this->height)
+                    ->save(
+                        storage_path("app/publics/$this->path/$nameFile"),
+                        70
+                    );
                 if (!$upload) {
                     throw new Exception("Uplado of file fail!");
                 } else {
@@ -59,12 +60,7 @@ class MainApiController extends BaseController
             $data = $this->model->create($dataForm);
             return response()->json($data, 201);
         } catch (Exception $error) {
-            $error_msg = ['error'=>$error->getMessage()];
-
-            if($error instanceof ValidationException)
-                array_push($error_msg,['msg:'=>$error->errors()]);
-
-            return response()->json($error_msg, 400);
+            $this->mainApiError($error);
         }
     }
 
@@ -92,13 +88,12 @@ class MainApiController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        if (!$model = $this->model->find($id)) {
-            return response()->json(['error' => 'Id inválido!'], 404);
-        }
-        $this->validate($this->req, $this->model->rules());
-        $dataForm = $this->req->all();
-        // return response()->json($dataForm,201);
         try {
+            if (!$model = $this->model->find($id)) {
+                return response()->json(['error' => 'Id inválido!'], 404);
+            }
+            $this->validate($this->req, $this->model->rules());
+            $dataForm = $this->req->all();
             if ($this->req->hasFile($this->upload)) { // && $this->req->file($this->uplaod)->isValid()){
                 $extension = $this->req->file($this->upload)->extension();
                 $imgName = uniqid(date('His')); //Unique ID based on date hour, minute and seconds.
@@ -118,12 +113,11 @@ class MainApiController extends BaseController
             //  else {
             //     return response()->json(['error' => 'Upload image fail!'], 400);
             // }
+            $model->update($dataForm);
+            return response()->json($model);
         } catch (Exception $error) {
-            return response()->json(['error' => $error->getMessage()], 400);
+           $this->mainApiError($error);
         }
-
-        $model->update($dataForm);
-        return response()->json($model);
     }
 
     /**
@@ -142,5 +136,15 @@ class MainApiController extends BaseController
 
         $model->delete();
         return response()->json(['success' => 'registro removido!']);
+    }
+
+    public function mainApiError(Exception $error)
+    {
+        $error_msg = ['error' => $error->getMessage()];
+
+        if ($error instanceof ValidationException)
+            array_push($error_msg, ['msg:' => $error->errors()]);
+
+        return response()->json($error_msg, 400);
     }
 }
