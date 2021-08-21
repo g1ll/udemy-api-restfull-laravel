6519be73134f43d4dba51d17b1a858e715955623
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-21 19:06:14 -0300

    Dump of locacaos inserts

---
+++ database/bkps/locacaos_inserts.sql
@@ -0,0 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 21, 2021 at 06:37 PM
+-- Server version: 8.0.26-0ubuntu0.20.04.2
+-- PHP Version: 8.0.9
+
+INSERT INTO `locacaos` (`id`, `cliente_id`, `filme_id`, `data_locacao`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2021-08-18', '2021-08-19 00:29:49', '2021-08-19 00:29:49');
