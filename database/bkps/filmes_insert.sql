0c6f04939b978ede19a2ac62c03599972d96c6a3
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-21 18:38:50 -0300

    Inserts for table filmes.

---
+++ database/bkps/filmes_insert.sql
@@ -0,0 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 21, 2021 at 06:36 PM
+-- Server version: 8.0.26-0ubuntu0.20.04.2
+-- PHP Version: 8.0.9
+
+
+INSERT INTO `filmes` (`id`, `titulo`, `capa`, `created_at`, `updated_at`) VALUES
+(1, 'Constelação de Orion from Hubble', '211451611da27b90a1a.jpg', '2021-08-19 00:14:51', '2021-08-19 00:14:51');
