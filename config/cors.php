a7e4291fa6e510fa7f355a303982e2a590ef7b5d
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-20 22:14:20 -0300

    Changing  CORS config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+    'allowed_origins' => ['http://devs'],
 
     'allowed_origins_patterns' => [],
 
