61742944baff1938f29b0f6896b8a598e2d7e4c7
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-24 20:29:10 -0300

    Create route unauthorized to abort reques with 401

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('unauthorized',function(){
+    // return response()->json(['error'=>'Unauthorized!'],401);
+    abort(401);
+})->name('unauthorized');
