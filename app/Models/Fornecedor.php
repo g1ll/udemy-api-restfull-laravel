880f07ae076b297de2eb9ba27051a5359a33aa80
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-08-24 21:41:12 -0300

    Model for fornecedo, create properties and methods

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores';
+    protected $primaryKey = 'id_fornecedore';
+    public $timestamp = false;
+
+    protected $fillable = [
+        'nome',
+    ];
+
+    public function rules()
+    {
+        return [
+            'nome'=>'required',
+        ];
+    }
+
+
 }
